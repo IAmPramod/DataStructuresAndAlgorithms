ea7a2d1db5337970a9a81c34d374dfdf79ae79eb
Pramod Gupta <gupta.pramod@live.com>
2018-08-18 10:31:56 +0530

    Modified description

--- LeetCode/Easy/Q746_MinCostClimbingStairs.java
+++ LeetCode/Easy/Q746_MinCostClimbingStairs.java
@@ -4,25 +4,21 @@
  * 
  * Approach: Used Dynamic Programming's bottom up approach
  * 
- * @author Pramod Gupta
+ * @author pramod
  * */
 
 
 class Q746_MinCostClimbingStairs {
-	String name = "Pramod"
-			public void printName(String val) {
-		name = name+"Gupta";
-	}
     public int minCostClimbingStairs(int[] cost) {
-//        int length = cost.length;
-//        if(length==1)
-//            return cost[0];
-//        int[] dp = new int[length];
-//        dp[0] = cost[0];
-//        dp[1] = cost[1];
-//        for(int i =2;i<length;i++){
-//            dp[i] = cost[i]+Math.min(dp[i-1],dp[i-2]);
-//        }
-//        return Math.min(dp[length-1],dp[length-2]);
+        int length = cost.length;
+        if(length==1)
+            return cost[0];
+        int[] dp = new int[length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i =2;i<length;i++){
+            dp[i] = cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[length-1],dp[length-2]);
     }
 }
